7827cae9c807bc4cf77b92b8371ed7912c5594bc
AnnaStevi <stevianawudandari464@gmail.com>
2023-10-17 08:17:00 +0700

    feat : app loundry

---
+++ src/apk_laundry_ukk/Apk_laundry_ukk.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package apk_laundry_ukk;
+
+/**
+ *
+ * @author stevi
+ */
+public class Apk_laundry_ukk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        koneksi k = new koneksi();
+        k.connect();
+    }
+    
+}
